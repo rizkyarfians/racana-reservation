d9716a6756b65ca6d4da8a2263e859fb17fbc120
rizkyarfians <rizkyarfians27@gmail.com>
2026-09-23 22:49:28 +0700

    Preserve semantics for polymorphic buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -43,11 +43,15 @@ function Button({
   className,
   variant = "default",
   size = "default",
+  nativeButton,
+  render,
   ...props
 }: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
   return (
     <ButtonPrimitive
       data-slot="button"
+      nativeButton={nativeButton ?? !render}
+      render={render}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
